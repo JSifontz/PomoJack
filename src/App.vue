2a76cf79ff1353fa19e00d384c2d345cdfe77564
Omar Crespo <crespomerchano@gmail.com>
2018-02-06 19:27:48 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <span class="message">{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to the PomoJack begin'
+    }
+  }
+}
+</script>
+
+<style>
+  .message {
+    color: peru;
+    font-size: 5vw
+  }
+
+  #app {
+    height: 97vh;
+    width: 99vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
